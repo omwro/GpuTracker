6a512630944ae278de594d957bd8cb6458f3a828
Omer Erdem <omer.erdem@hva.nl>
2021-02-14 22:50:28 +0100

    Alternate implementation

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
 const puppeteer = require('puppeteer');
 const open = require('open');
 
+const shippingInfo = {
+    "email": "EMAIL@GMAIL.COM",
+    "firstname": "FIRSTNAME",
+    "lastname": "LASTNAME",
+    "street": "STREET",
+    "housenumber": "123",
+    "zipcode": "1234AB",
+    "city": "CITY",
+    "phone": "0612345678",
+    "bank": "INGBNL2A"
+}
+
 //// The queries for the stores
 const stores = {
     "Alternate": {
+        "name": "Alternate",
         "query": [".stockStatus"],
         "stockmsg": ["Direct leverbaar", "Levertijd"],
-        "addToCartBtn": ""
+        "addToCart": ".button.accept-32",
+        "checkoutUrl": "https://www.alternate.nl/html/checkout/order.html",
     },
     "Azerty": {
+        "name": "Azerty",
         "query": [".stockchecked:first-child", ".stockchecked:nth-child(2)"],
         "stockmsg": 0
     },
     "Amazon": {
+        "name": "Amazon",
         "query": ["#availability span"],
         "stockmsg": ["Op voorraad.", "Nog slechts"]
     },
     "Megekko": {
+        "name": "Megekko",
         "query": [".product_detail .subtitle"],
         "stockmsg": ["Uit eigen voorraad leverbaar."]
     }
@@ -27,7 +44,7 @@ const products = [
         "name": "USB",
         "store": "Alternate",
         "url": "https://www.alternate.nl/SanDisk/Blade-16-GB-usb-stick/html/product/147756?"
-    },{
+    }, {
         "name": "MSI RTX 3060 TI €549",
         "store": "Alternate",
         "url": "https://www.alternate.nl/MSI/GeForce-RTX-3060-Ti-VENTUS-2X-OC-grafische-kaart/html/product/1698708?"
@@ -50,33 +67,27 @@ const products = [
     //// proxy's from https://hidemy.name/en/proxy-list/?country=NL#list
     let launchOptions = {
         headless: false,
-        args: ['--proxy-server=37.120.239.151:3128']
+        args: ['--start-maximized', '--proxy-server=37.120.239.151:3128']
     };
     const browser = await puppeteer.launch(launchOptions);
     const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080});
+
 
     for (let i = 0; i < products.length; i++) {
-        await page.goto(products[i].url);
-        const store = stores[products[i].store]
-
-        for (let query of store['query']) {
-            let text = await page.evaluate((query) => Array.from(document.querySelectorAll(query), element => element.textContent), query);
-            if (Array.isArray(store['stockmsg'])) {
-                text = text.toString().trim();
-                let isInStock = false
-                for (const msg of store['stockmsg']) {
-                    if (text.startsWith(msg)) isInStock = true;
-                }
-                onResult(products[i], isInStock);
-                if (isInStock) i = 100
-            } else if (store['stockmsg'] === 0) {
-                let qty = parseInt(text.toString().replace(/\D/g, ''));
-                if (qty > 0) {
-                    onResult(products[i], true);
-                    i = 100
-                }
-                else onResult(products[i], false);
-            } else onResult(products[i], false);
+        const product = products[i]
+        const store = stores[product.store]
+        await page.goto(product.url);
+        const stockStatus = await getProductStockStatus(page, store)
+        logResult(product, stockStatus)
+        if (stockStatus) {
+            i = 100
+            await page.$eval(store['addToCart'], form => form.click());
+            await page.waitForNavigation();
+            await page.goto(store['checkoutUrl']);
+            await fillInCheckout(page, store)
+            // await page.waitForNavigation();
+            // await page.$eval(store['goToCheckout'], form => form.click());
         }
 
         if (i === products.length - 1) i = -1;
@@ -86,7 +97,38 @@ const products = [
     // await browser.close();
 })();
 
-function onResult(product, bool) {
+async function getProductStockStatus(page, store) {
+    for (let query of store['query']) {
+        let text = await page.evaluate((query) => Array.from(document.querySelectorAll(query), element => element.textContent), query);
+        if (Array.isArray(store['stockmsg'])) {
+            text = text.toString().trim();
+            for (const msg of store['stockmsg']) {
+                if (text.startsWith(msg)) return true;
+            }
+        } else if (store['stockmsg'] === 0) {
+            let qty = parseInt(text.toString().replace(/\D/g, ''));
+            if (qty > 0) return true
+        }
+    }
+    return false
+}
+
+async function fillInCheckout(page, store) {
+    if (store.name === "Alternate") {
+        await page.type("#firstname", shippingInfo['firstname'])
+        await page.type("#surname", shippingInfo['lastname'])
+        await page.type("#street", shippingInfo['street'] + " " + shippingInfo['housenumber'])
+        await page.type("#zip", shippingInfo['zipcode'])
+        await page.type("#city", shippingInfo['city'])
+        await page.type("#phone", shippingInfo['phone'])
+        await page.type("#email", shippingInfo['email'])
+        await page.select('select#paymetIssuerIngIdeal', shippingInfo['bank'])
+        await page.evaluate(() => {document.querySelector("#terms").click();});
+    }
+
+}
+
+function logResult(product, bool) {
     if (bool) {
         windowsSound()
         console.log("\x1b[32m", product.store, "/", product.name, "=", "IN STOCK!")
@@ -96,6 +138,14 @@ function onResult(product, bool) {
     } else {
         console.log("\x1b[31m", product.store, "/", product.name, "=", "out of stock")
     }
+}
+
+function addToCart(page, store) {
+    page.$eval(store['addToCart'], form => form.click());
+}
+
+function goToCart(page, store) {
+    page.goto(store['cartUrl'], {waitUntil: 'networkidle0'});
 }
 
 function delay(time) {
